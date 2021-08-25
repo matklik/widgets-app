0791e001d492dc8469eaa31220d5ed7322bcb173
Mathew Klik <mathew.klik@harveynichols.com>
2021-08-25 17:18:44 +0100

    Lint errors fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,17 @@ import Link from './Link';
 
 const Header = ({ items }) => {
     
+    // eslint-disable-next-line no-unused-vars
     const [activeIndex, setActiveIndex] = useState(0);
 
+    // eslint-disable-next-line no-unused-vars
+    const onItemClick = (index) => {
+        setActiveIndex(index);
+    };
+
     const headerMenuItems = items.map((item, index) => {
+        // eslint-disable-next-line no-unused-vars
+        const active = window.location.pathname === item.href ? 'active' : '';
 
         return (<React.Fragment key={item.name}>
             <Link href={item.link} className="item">{item.name}</Link>
